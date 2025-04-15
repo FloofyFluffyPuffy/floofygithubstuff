62e8788aeb5a32c9048ccea0ca443228e6fe2afa
Floofy <jimmycrabs2006@gmail.com>
2025-04-15 14:27:29 +0700

    v0.01

---
+++ reactapp1/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="heading">Taskify</div>
+    </div>
+  );
+}
+export default App;
