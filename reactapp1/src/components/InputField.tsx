5244bfeecf639ac2a70feb6bb3bf66b8f4c707af
Floofy <jimmycrabs2006@gmail.com>
2025-04-16 16:28:09 +0700

    v0.03

--- reactapp1/src/components/InputField.tsx
+++ reactapp1/src/components/InputField.tsx
@@ -1,10 +1,12 @@
 // inputfield.tsx
 import React from 'react';
+import './style.css';
 
 const InputField = () => {
   return (
     <form className='input'>
-      <input type="text" placeholder='Enter a task'/>
+      <input type="text" placeholder='Enter a task' className='inputBox'/>
+      <button className='inputSubmit' type='submit'> </button>
     </form>
   );
 };
