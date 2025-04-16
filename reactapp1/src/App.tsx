fbf2d66ef7fa410954f1e8e93844ce07664f790f
Floofy <jimmycrabs2006@gmail.com>
2025-04-16 15:50:33 +0700

    v 0.02

--- reactapp1/src/App.tsx
+++ reactapp1/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import InputField from './components/InputField';
 const App: React.FC = () => {
   return (
     <div className="App">
       <div className="heading">Taskify</div>
+      <InputField />
     </div>
   );
 }
---
+++ reactapp1/src/components/InputField.tsx
@@ -0,0 +1,12 @@
+// inputfield.tsx
+import React from 'react';
+
+const InputField = () => {
+  return (
+    <form className='input'>
+      <input type="text" placeholder='Enter a task'/>
+    </form>
+  );
+};
+
+export default InputField;
