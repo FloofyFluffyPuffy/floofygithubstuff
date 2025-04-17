3716c3b74ec8fa5ea692229a2507eaa946bb6164
Floofy <jimmycrabs2006@gmail.com>
2025-04-17 20:04:22 +0700

    v0.04

--- reactapp1/src/App.tsx
+++ reactapp1/src/App.tsx
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/inputField';
-
+import { Todo } from './components/model';
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-  console.log(todo)
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault();
+    if (todo)
+    {
+      setTodos([...todos, {id: Date.now(), todo:todo, isDone:false}])
+      setTodo("");
+    }
+
+  }
+  console.log(todos)
   return (
     <div className="App">
       <div className="heading">Taskify</div>
-      <InputField todo= {todo} setTodo={setTodo} />
+      <InputField todo= {todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
